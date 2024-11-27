63725a2924ca27e72d39bf81be3fa98f79a42dee
tsukuyomi-07 <sebas.07d@gmail.com>
2024-11-27 18:41:08 -0500

    2

--- modelo/crud.php
+++ modelo/crud.php
@@ -3,8 +3,10 @@
 include "../conexion.php";
 
 $conexion->query("INSERT INTO users VALUES(
-    null,
-    '$_POST[names]',
-    $_POST[document],
-    1
+    null, '$_POST[names]', $_POST[document], 1
 )");
+?>
+
+<html>
+<button><a href="http://localhost/clase-php-1/view/index.php">Regresar al inicio</a></button>
+</html>
